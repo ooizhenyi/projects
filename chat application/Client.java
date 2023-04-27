41d747806006f47a889e6f20c32ce6146cbc8549
Jerry Ooi <56662757+ooizhenyi@users.noreply.github.com>
2023-04-27 15:30:11 +0800

    code change

--- chat application/Client.java
+++ chat application/Client.java
@@ -1,26 +1,93 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to the chat client!");
-        System.out.print("Enter server address: ");
-        Scanner console = new Scanner(System.in);
-        String serverAddress = console.nextLine();
-        Socket socket = new Socket(serverAddress, 59001);
-        Scanner in = new Scanner(socket.getInputStream());
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        while (true) {
-            String line = console.nextLine();
-            if (line.toLowerCase().startsWith("/quit")) {
-                break;
+    private static final int SERVER_PORT = 12345;
+    private static final String SERVER_ADDRESS = "localhost";
+
+    private static String username;
+    private static String password;
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get username and password from user
+        System.out.print("Enter your username: ");
+        username = scanner.nextLine();
+        System.out.print("Enter your password: ");
+        password = scanner.nextLine();
+
+        // Create socket connection to server
+        Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+
+        // Create input and output streams
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+        // Send authentication information to server
+        out.writeObject(username);
+        out.writeObject(password);
+
+        // Wait for server response
+        boolean isAuthenticated;
+        try {
+            isAuthenticated = (boolean) in.readObject();
+            if (isAuthenticated) {
+                System.out.println("Successfully authenticated as " + username);
+    
+                // Start sending and receiving messages
+                new Thread(new ClientSender(out)).start();
+                new Thread(new ClientReceiver(in)).start();
+            } else {
+                System.out.println("Authentication failed. Please try again.");
             }
-            out.println(line);
-            String response = in.nextLine();
-            System.out.println(response);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } 
+
+
+    }
+}
+
+class ClientSender implements Runnable {
+    private ObjectOutputStream out;
+
+    public ClientSender(ObjectOutputStream out) {
+        this.out = out;
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            while (true) {
+                // Read user input and send message to server
+                String message = scanner.nextLine();
+                out.writeObject(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        socket.close();
+    }
+}
+
+class ClientReceiver implements Runnable {
+    private ObjectInputStream in;
+
+    public ClientReceiver(ObjectInputStream in) {
+        this.in = in;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                // Read incoming messages from server and display to user
+                String message = (String) in.readObject();
+                System.out.println(message);
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- chat application/Server.java
+++ chat application/Server.java
@@ -1,61 +1,103 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
 public class Server {
+    private static Vector<ClientHandler> clients = new Vector<>();
 
-    private static ArrayList<PrintWriter> clients = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        System.out.println("Server started on port 1234");
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("The chat server is running.");
-        int clientNumber = 0;
-        ServerSocket listener = new ServerSocket(59001);
-        try {
-            while (true) {
-                new Handler(listener.accept(), clientNumber++).start();
+        while (true) {
+            Socket socket = null;
+
+            try {
+                socket = serverSocket.accept();
+                System.out.println("New client connected: " + socket);
+
+                DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+
+                System.out.println("Creating new user...");
+
+                String username = inputStream.readUTF();
+                String password = inputStream.readUTF();
+
+                if (authenticateUser(username, password)) {
+                    System.out.println("User authenticated: " + username);
+                    ClientHandler client = new ClientHandler(socket, username, inputStream, outputStream);
+                    Thread thread = new Thread(client);
+                    clients.add(client);
+                    thread.start();
+                } else {
+                    System.out.println("User authentication failed: " + username);
+                    outputStream.writeUTF("Authentication failed");
+                    socket.close();
+                }
+
+            } catch (Exception e) {
+                socket.close();
+                e.printStackTrace();
             }
-        } finally {
-            listener.close();
         }
     }
 
-    private static class Handler extends Thread {
-        private String name;
-        private Socket socket;
-        private Scanner in;
-        private PrintWriter out;
+    private static boolean authenticateUser(String username, String password) {
+        // Implement your authentication mechanism here
+        // For simplicity, we will just hardcode a single user
+        return username.equals("user") && password.equals("password");
+    }
 
-        public Handler(Socket socket, int clientNumber) {
+    static class ClientHandler implements Runnable {
+        private String name;
+        final DataInputStream inputStream;
+        final DataOutputStream outputStream;
+        Socket socket;
+        boolean isLoggedIn;
+
+        public ClientHandler(Socket socket, String name, DataInputStream inputStream, DataOutputStream outputStream) {
             this.socket = socket;
-            name = "Client " + clientNumber;
+            this.name = name;
+            this.inputStream = inputStream;
+            this.outputStream = outputStream;
+            this.isLoggedIn = true;
         }
 
+        @Override
         public void run() {
-            try {
-                in = new Scanner(socket.getInputStream());
-                out = new PrintWriter(socket.getOutputStream(), true);
+            String received;
 
-                clients.add(out);
-
-                while (true) {
-                    String input = in.nextLine();
-                    if (input.toLowerCase().startsWith("/quit")) {
-                        return;
-                    }
-                    for (PrintWriter client : clients) {
-                        client.println(name + ": " + input);
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println(e);
-            } finally {
-                clients.remove(out);
+            while (true) {
                 try {
-                    socket.close();
+                    received = inputStream.readUTF();
+
+                    if (received.equals("logout")) {
+                        this.isLoggedIn = false;
+                        this.socket.close();
+                        break;
+                    }
+
+                    sendMessageToAllClients(received);
+
                 } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                this.inputStream.close();
+                this.outputStream.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void sendMessageToAllClients(String message) throws IOException {
+            for (ClientHandler client : clients) {
+                if (client.isLoggedIn) {
+                    client.outputStream.writeUTF(this.name + ": " + message);
                 }
             }
         }
